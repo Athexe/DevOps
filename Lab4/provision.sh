a8c08808eb44d04a3086535a3dcb5ec3a99fe73a
Ihor Klymenko <ihor.klymenko1@gmail.com>
2021-12-05 16:40:33 +0200

    Lab 5

--- Lab4/provision.sh
+++ Lab4/provision.sh
@@ -1,8 +1,10 @@
 sudo useradd adminuser                        
 echo adminuser | passwd adminuser --stdin     
-sudo usermod -aG wheel adminuser              
+sudo usermod -aG wheel adminuser  
+
 sudo useradd poweruser                        
 echo poweruser | passwd poweruser --stdin     
 sudo usermod -aG wheel poweruser              
 setfacl -m g:poweruser:rx /home/adminuser   
+
 find / -perm /4000 
